0a142df1b5031faf9f38347a80036da270a3407f
Rafal Kociszewski <rafal.kociszewski@gmail.com>
2017-07-06 18:03:01 +0200

    convert indention to spaces

--- test/postal_code/data_parser_test.exs
+++ test/postal_code/data_parser_test.exs
@@ -1,14 +1,14 @@
 defmodule ElhexDelivery.PostalCode.DataParserTest do
-	use ExUnit.Case
-	alias ElhexDelivery.PostalCode.DataParser
-	doctest ElhexDelivery
+  use ExUnit.Case
+  alias ElhexDelivery.PostalCode.DataParser
+  doctest ElhexDelivery
 
-	# Just check whether postal code file is not empty
-	test "parse stuff" do
-		geo_data = DataParser.parse_data
-		{latitude, longitude} = Map.get(geo_data, "00601")
-		
-		assert is_float(latitude)
-		assert is_float(longitude)
-	end
+  # Just check whether postal code file is not empty
+  test "parse stuff" do
+    geo_data = DataParser.parse_data
+    {latitude, longitude} = Map.get(geo_data, "00601")
+    
+    assert is_float(latitude)
+    assert is_float(longitude)
+  end
 end
