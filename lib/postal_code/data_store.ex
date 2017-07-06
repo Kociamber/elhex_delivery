9920e3a9d62a259a9e9746b8364fc1b1fd5596c0
Rafal Kociszewski <rafal.kociszewski@gmail.com>
2017-07-06 18:37:26 +0200

    fix indentions

--- lib/postal_code/data_store.ex
+++ lib/postal_code/data_store.ex
@@ -1,26 +1,26 @@
 defmodule ElhexDelivery.PostalCode.DataStore do
-	use GenServer
-	alias ElhexDelivery.PostalCode.DataParser
+  use GenServer
+  alias ElhexDelivery.PostalCode.DataParser
 
-	## GenServer client API
+  ## GenServer client API
 
-	# %{} is pointing at GenServer's state typ while name provides process name. Naming a process 
-	def start_link do
-		GenServer.start_link(__MODULE__, %{}, name: :data_store)
-	end
+  # %{} is pointing at GenServer's state typ while name provides process name. Naming a process 
+  def start_link do
+    GenServer.start_link(__MODULE__, %{}, name: :data_store)
+  end
 
-	def get_geolocation(postal_code) do
-		GenServer.call(:data_store, {:get_geolocation, postal_code})
-	end
+  def get_geolocation(postal_code) do
+    GenServer.call(:data_store, {:get_geolocation, postal_code})
+  end
 
-	## GenServer server API / callbacks
+  ## GenServer server API / callbacks
 
-	def init(_) do
-		{:ok, DataParser.parse_data}
-	end
+  def init(_) do
+    {:ok, DataParser.parse_data}
+  end
 
-	def handle_call({:get_geolocation, postal_code}, _from, geo_data) do
-		geo_location = Map.get(geo_data, postal_code)
-		{:reply, geo_location, geo_data}
-	end
+  def handle_call({:get_geolocation, postal_code}, _from, geo_data) do
+    geo_location = Map.get(geo_data, postal_code)
+    {:reply, geo_location, geo_data}
+  end
 end
