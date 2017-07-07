33a8cb6278fc0e96870a824bdf9d838148946e3b
Rafal Kociszewski <rafal.kociszewski@gmail.com>
2017-07-07 11:12:37 +0200

    add distance calculation and cover it with tests

--- lib/postal_code/navigator.ex
+++ lib/postal_code/navigator.ex
@@ -1,5 +1,9 @@
 defmodule ElhexDelivery.PostalCode.Navigator do
   alias ElhexDelivery.PostalCode.DataStore
+  alias :math, as: Math
+
+  @radius_km 6371
+  # @radius_mi 3959
 
   def get_distance(from, to) do
     do_get_distance(from, to)
@@ -30,7 +34,27 @@ defmodule ElhexDelivery.PostalCode.Navigator do
     raise ArgumentError, error
   end
 
-  defp calculate_distance({lat1, long1}, {lat2, long2}) do
-    
+   defp calculate_distance({lat1, long1}, {lat2, long2}) do
+    lat_diff = degrees_to_radians(lat2 - lat1)
+    long_diff = degrees_to_radians(long2 - long1)
+
+    lat1 = degrees_to_radians(lat1)
+    lat2 = degrees_to_radians(lat2)
+
+    cos_lat1 = Math.cos(lat1)
+    cos_lat2 = Math.cos(lat2)
+
+    sin_lat_diff_sq = Math.sin(lat_diff / 2) |> Math.pow(2)
+    sin_long_diff_sq = Math.sin(long_diff / 2) |> Math.pow(2)
+
+    a = sin_lat_diff_sq + (cos_lat1 * cos_lat2 * sin_long_diff_sq)
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+    @radius_km * c |> Float.round(2)
   end
+
+  defp degrees_to_radians(degrees) do
+    degrees * (Math.pi / 180)
+  end
+
 end
--- test/postal_code/navigator_test.ex
+++ test/postal_code/navigator_test.ex
@@ -6,18 +6,48 @@ defmodule ElhexDelivery.PostalCode.NavigatorTest do
   describe "get_distance format tests" do
     test "postal codes are strings" do
       distance = Navigator.get_distance("00601", "94062")
+      assert is_float(distance)
     end
 
     test "postal codes are integers" do
       distance = Navigator.get_distance(00601, 94062)
+      assert is_float(distance)
     end
 
     test "postal codes are mixed types" do
       distance = Navigator.get_distance("00601", 94062)
+      assert is_float(distance)
     end
 
     test "postal codes are in unexpected format" do
       assert_raise ArgumentError, fn -> distance = Navigator.get_distance("Q%%%&*(*&", 94.062) end
+    end
+  end
+
+  describe "get_distance (actual distance)" do
+    test "distance_between_rwc_and_sf" do
+      # Redwood City, CA 94062
+      # San Francisco, CA 94104
+      # Driving distance ~ 40 miles
+      distance = Navigator.get_distance(94062, 94104)
+      # IO.puts "RWC -> SF: #{distance}"
+      assert distance == 26.75
+    end
+    test "distance_between_sf_and_nyc" do
+      # San Francisco, CA 94104
+      # New York, NY 10112
+      # Driving distance ~ 3100 miles
+      distance = Navigator.get_distance(94104, 10112)
+      # IO.puts "SF -> NY: #{distance}"
+      assert distance == 2565.28
+    end
+    test "distance_between_mnpls_and_austin" do
+      # Minneapolis, MN 55401
+      # Austin, TX 78703
+      # Driving distance ~ 1100 miles
+      distance = Navigator.get_distance(55401, 78703)
+      # IO.puts "MNPLS -> Austin: #{distance}"
+      assert distance == 1044.08
     end
   end
 end
